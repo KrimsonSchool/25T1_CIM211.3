83cda0aab51d64f2a5c0e969349cac444e88b027
JAIS_PC\jaivd <jaivda111@gmail.com>
2025-04-03 15:25:45 +1000

    Tiny Sanity thing

--- Assets/Sanity.cs
+++ Assets/Sanity.cs
@@ -6,15 +6,19 @@ public class Sanity : MonoBehaviour
     public PostProcessVolume ppv;
 
     private Vignette vg;
+    private ChromaticAberration chrom;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        ppv.profile.TryGetSettings(out vg);
+        ppv.profile.TryGetSettings(out vg);//starts 45%
+        ppv.profile.TryGetSettings(out chrom);//starts 50%
+        //grain                               //starts 50%
     }
 
     // Update is called once per frame
     void Update()
     {
         vg.intensity.value += Time.deltaTime/1000;
+        chrom.intensity.value += Time.deltaTime/1000;
     }
 }
