b4a6cde314aa331f0961b4d2cf0ee673d83102cd
Dakota E <62161190+Kaizenoku@users.noreply.github.com>
2025-04-24 00:18:14 +1000

    Minor fixes
    - Fixed player being able look too far up/down
    - Fixed "URBAD" not letting you click pause menu buttons if enabled (just changed the canvas sorting order to -1)
    - Renamed "CassetTape" to "Casette Tape"

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -13,6 +13,9 @@ public class Player : MonoBehaviour
 
     private float _defaultGrainSize;
     private float _defaultGrainIntensity;
+
+    private float _mousePitch;
+    
     //STANDARDS:
     //PUBLIC = SOMETHING PLAYER CAN EDIT
     //PRIVATE = SOMETHING PLAYER CAN'T EDIT
@@ -53,8 +56,12 @@ public class Player : MonoBehaviour
         transform.position += (transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime) + (transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
         
         transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime, 0));
+
+        float mouseY = Input.GetAxis("Mouse Y") * rotSpeed * Time.deltaTime;
+        _mousePitch = Mathf.Clamp(_mousePitch - mouseY, -85f, 85f);
+        Camera.main.transform.localEulerAngles = new Vector3(_mousePitch, 0, 0);
         
-        Camera.main.gameObject.transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y") * rotSpeed * Time.deltaTime,0, 0));
+        // Camera.main.gameObject.transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y") * rotSpeed * Time.deltaTime,0, 0));
 
         if (Input.GetMouseButtonDown(1))
         {
